a18eedfcf297e47ee93f81c8a0e9cf1d5c359ce4
Andres Cabrera <andresexplorer83@gmail.com>
2024-02-22 03:41:06 -0300

    materias primas crud in development, added basic but practical navigation links :)

--- app.py
+++ app.py
@@ -27,6 +27,21 @@ class Fornecedores(db.Model):
     email_vendedor = db.Column(db.String(100))
 
 
+class MateriasPrimas(db.Model):
+    __tablename__ = "materiasprimas"
+    __table_args__ = {"extend_existing": True}
+
+    id_mp = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome_mp = db.Column(db.String(45))
+    unidade_mp = db.Column(db.Enum('KG', 'UN'))
+    peso_mp = db.Column(db.Numeric(10, 3))
+    quantidade_mp = db.Column(db.Integer)
+    custo_mp = db.Column(db.Numeric(10, 3))
+    departamento_mp = db.Column(db.Enum('Carnes', 'Farinhas', 'Hortifruti', 'Mercearia', 'Misturas', 'Ovos', 'Queijos'))
+    pedidomin_mp = db.Column(db.Integer)
+    gastomedio_mp = db.Column(db.Numeric(10, 3))
+    id_fornecedor = db.Column(db.Integer)
+
 
     
 # Schema for serialization
@@ -43,6 +58,22 @@ class FornecedorSchema(ma.SQLAlchemySchema):
     contato_tel = ma.auto_field()
     email_vendedor = ma.auto_field()
 
+
+class MateriasPrimasSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = MateriasPrimas
+
+    id_mp = ma.auto_field()
+    nome_mp = ma.auto_field()
+    unidade_mp = ma.auto_field()
+    peso_mp = ma.auto_field()
+    quantidade_mp = ma.auto_field()
+    custo_mp = ma.auto_field()
+    departamento_mp = ma.auto_field()
+    pedidomin_mp = ma.auto_field()
+    gastomedio_mp = ma.auto_field()
+    id_fornecedor = ma.auto_field()
+
     
 
 
@@ -54,6 +85,15 @@ with app.app_context():
 @app.route('/')
 def index():
     return "Hello World"
+
+
+@app.route('/materiasprimas')
+def materiasPrimas():
+
+    fornecedores = Fornecedores.query.all() #get values from fornecedores table
+    fornecedor_schema = FornecedorSchema(many=True)
+    serialized_data = fornecedor_schema.dump(fornecedores)
+    return render_template('materiasprimas.html', fornecedores=serialized_data)
 
 
 @app.route('/cleanup-fornecedores', methods=['GET'])
@@ -75,7 +115,7 @@ def fornecedores():
         contato_tel = request.form.get('contato_tel')
         email_vendedor = request.form.get('email_vendedor')
         
-        entry = Fornecedores(
+        fornecedores = Fornecedores(
             nome_fornecedor=nome_fornecedor, 
             tempo_entrega=tempo_entrega,
             prazo_pagamento=prazo_pagamento,
@@ -84,7 +124,7 @@ def fornecedores():
             contato_tel=contato_tel,
             email_vendedor=email_vendedor
             )
-        db.session.add(entry)
+        db.session.add(fornecedores)
         db.session.commit()
         return redirect(url_for('fornecedores'))  # Redirect to GET request after form submission
 
