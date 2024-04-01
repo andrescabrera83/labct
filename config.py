8c5a915e6ef79deb8633eb3160d5d18a84f441ca
Andres Cabrera <andresexplorer83@gmail.com>
2024-03-31 21:56:25 -0300

    secret key location changed

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 from datetime import timedelta
+import os
 
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     PERMANENT_SESSION_LIFETIME = timedelta(days=62)
-    SECRET_KEY = 'SOXOZvMJBD'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG = False
