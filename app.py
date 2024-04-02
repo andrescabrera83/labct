6c8e028478d70d5e0d6780ccfdbbb87525d62a42
Andres Cabrera <andresexplorer83@gmail.com>
2024-04-01 21:24:11 -0300

    history filer actually updated

--- app.py
+++ app.py
@@ -649,7 +649,7 @@ app.jinja_env.filters['custom_datetime_format'] = custom_datetime_format
 
 def custom_datetime_format2(value):
     if isinstance(value, str):
-        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S')
+        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
     formatted_date = value.strftime("%Y-%m-%d (%H:%M)")
     return formatted_date
 app.jinja_env.filters['custom_datetime_format2'] = custom_datetime_format2
